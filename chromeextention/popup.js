a58816bd0b60bbce8024908f2b517149c98be0c6
shallaa <shallaa@shallaazm.com>
2014-04-17 00:38:40 +0900

    크롬 익스텐션 추가.

--- chromeextention/popup.js
+++ chromeextention/popup.js
@@ -1,9 +1,9 @@
 document.addEventListener( 'DOMContentLoaded', function(){
 	chrome.tabs.getSelected( null, function(tab){
 		var xhr = new XMLHttpRequest(), c, d;
-		xhr.open("GET", 'http://jsonp.ligo.kr/?callback=callback&url=' + encodeURIComponent(tab.url), false), xhr.send(),
-		d = JSON.parse( xhr.responseText.substring('callback('.length, xhr.responseText.length - 1 ) ).data,
-		document.getElementById('url').value = d, 
+		xhr.open("GET", 'http://api.ligo.kr/?url=' + encodeURIComponent(tab.url), false), xhr.send(),
+		d = xhr.responseText,
+		document.getElementById('url').value = d,
 		( c = document.getElementById('copy') ).innerHTML = 'Copy',
 		c.addEventListener( 'click', function(){
 			document.getElementById('url').select(),
