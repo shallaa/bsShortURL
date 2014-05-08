260b3152d789888d6b065daab2c6965239477906
shallaa <shallaa@shallaazm.com>
2014-05-09 01:42:57 +0900

    test2

--- django/ligoapp/views.py
+++ django/ligoapp/views.py
@@ -2,8 +2,4 @@
 from django.http import HttpResponse
 
 def main_page(request):
-<<<<<<< HEAD
     return HttpResponse('hello. this is test. test.')
-=======
-    return HttpResponse('hello. this is test. by ben')
->>>>>>> a09280af0da1d272c8af67a26d67bd4b608c9be3
